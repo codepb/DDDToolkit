e88064b608fca2d56d6e9c086ee9170acc554062
codepb <paul.j.burgess@outlook.com>
2018-01-14 20:02:14 +0000

    Creating Readable and Writable API Controllers. Resolves #6

---
+++ src/DDDToolkit.API/WritableAggregateController.cs
@@ -0,0 +1,56 @@
+﻿using DDDToolkit.ApplicationLayer;
+using DDDToolkit.Core.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DDDToolkit.API
+{
+    public abstract class WritableAggregateController<T, TId> : Controller, IWritableAggregateController<T, TId> where T : class, IAggregateRoot<TId>
+    {
+        private readonly IWritableApplicationService<T, TId> _applicationService;
+
+        protected WritableAggregateController(IWritableApplicationService<T, TId> applicationService)
+        {
+            _applicationService = applicationService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] T aggregate)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _applicationService.Add(aggregate);
+
+            return NoContent();
+        }
+
+        [HttpPut]
+        [Route("{id}")]
+        public async Task<IActionResult> Edit(TId id, [FromBody] T aggregate)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _applicationService.Update(id, aggregate);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+        [Route("{id}")]
+        public async Task<IActionResult> Delete(TId id)
+        {
+            await _applicationService.Delete(id);
+
+            return NoContent();
+        }
+    }
+}
