b760e8ee24a09a4690d7b615f2d4045158ad1dc0
codepb <paul.j.burgess@outlook.com>
2018-01-29 20:24:23 +0000

    removing specification project

--- src/DDDToolkit.Specifications/ISpecification.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DDDToolkit.Specifications
-{
-    public interface ISpecification<T>
-    {
-        ISpecification<T> And(ISpecification<T> other);
-        ISpecification<T> Or(ISpecification<T> other);
-        bool IsSatisfiedBy(T obj);
-    }
-}
