3f5a24cea36a8f01e3907e68b56799a9ff964a86
codepb <paul.j.burgess@outlook.com>
2018-02-03 18:43:37 +0000

    Fixing issue with tracking on Query methods, and adding a query with children

--- src/DDDToolkit.Repository.Sql/ReadableRepository.cs
+++ src/DDDToolkit.Repository.Sql/ReadableRepository.cs
@@ -25,7 +25,7 @@ namespace DDDToolkit.Repository.Sql
         protected virtual IQueryable<T> ApplyIncludes(IQueryable<T> query) => query.IncludeEverything();
         protected virtual IQueryable<T> Queryable => ApplyIncludes(Set.AsNoTracking());
 
-        public Task<IReadOnlyCollection<T>> GetAll(CancellationToken cancellationToken = default(CancellationToken)) => Set.ToListAsync(cancellationToken).ToReadOnlyCollection();
+        public Task<IReadOnlyCollection<T>> GetAll(CancellationToken cancellationToken = default(CancellationToken)) => Set.AsNoTracking().ToListAsync(cancellationToken).ToReadOnlyCollection();
 
         public Task<T> GetById(TId id, CancellationToken cancellationToken = default(CancellationToken))
         {
@@ -38,18 +38,25 @@ namespace DDDToolkit.Repository.Sql
             return Queryable.FirstOrDefaultAsync(eLambda, cancellationToken);
         }
 
+        private Task<IReadOnlyCollection<T>> ExecuteQueryOnQueryable(Expression<Func<T, bool>> query, IQueryable<T> queryable, CancellationToken cancellationToken)
+            => queryable.Where(query).ToReadOnlyCollectionAsync(cancellationToken);
+
         public Task<IReadOnlyCollection<T>> Query(Expression<Func<T, bool>> query, CancellationToken cancellationToken = default(CancellationToken))
-            => Set.Where(query).ToReadOnlyCollectionAsync(cancellationToken);
+            => ExecuteQueryOnQueryable(query, Set.AsNoTracking(), cancellationToken);
 
         public Task<IReadOnlyCollection<T>> Query(IQuery<T> query, CancellationToken cancellationToken = default(CancellationToken))
             => Query(query.AsExpression(), cancellationToken);
+
+        public Task<IReadOnlyCollection<T>> QueryWithChildren(Expression<Func<T, bool>> query, CancellationToken cancellationToken = default(CancellationToken))
+            => ExecuteQueryOnQueryable(query, Queryable, cancellationToken);
+
+        public Task<IReadOnlyCollection<T>> QueryWithChildren(IQuery<T> query, CancellationToken cancellationToken = default(CancellationToken))
+            => QueryWithChildren(query.AsExpression(), cancellationToken);
 
         public Task<T> FirstOrDefault(Expression<Func<T, bool>> query, CancellationToken cancellationToken = default(CancellationToken))
             => Queryable.FirstOrDefaultAsync(query, cancellationToken);
 
         public Task<T> FirstOrDefault(IQuery<T> query, CancellationToken cancellationToken = default(CancellationToken))
             => FirstOrDefault(query.AsExpression(), cancellationToken);
-
-
     }
 }
