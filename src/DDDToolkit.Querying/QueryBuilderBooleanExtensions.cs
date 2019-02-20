acb3618f4b00739a6ff0467df9d868ab832ba4b4
codepb <paul.j.burgess@outlook.com>
2019-02-20 08:06:39 +0000

    Adding boolean extensions

---
+++ src/DDDToolkit.Querying/QueryBuilderBooleanExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DDDToolkit.Querying
+{
+    public static class QueryBuilderBooleanExtensions
+    {
+        private static Query<T> BuildQuery<T>(ConstantExpression otherEntity, QueryBuilderExpression<T, bool> queryBuilderExpression)
+        {
+            var expression = Expression.Equal(queryBuilderExpression._expression.Body, otherEntity);
+            var lambda = Expression.Lambda<Func<T, bool>>(expression, queryBuilderExpression._expression.Parameters);
+            return queryBuilderExpression._continueWith(new Query<T>(lambda));
+        }
+
+        public static Query<T> True<T>(this QueryBuilderExpression<T, bool> queryBuilderExpression)
+            => BuildQuery(Expression.Constant(true, typeof(bool)), queryBuilderExpression);
+
+        public static Query<T> False<T>(this QueryBuilderExpression<T, bool> queryBuilderExpression)
+            => BuildQuery(Expression.Constant(false, typeof(bool)), queryBuilderExpression);
+    }
+}
---
+++ tests/DDDToolkit.Querying.Tests/Query_BooleanExtensionsShould.cs
@@ -0,0 +1,43 @@
+﻿using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DDDToolkit.Querying.Tests
+{
+    public class Query_BooleanExtensionsShould
+    {
+        [Fact]
+        public void TrueCorrectlyReturnsTrue()
+        {
+            var query = Query<bool>.Is.True();
+
+            true.Satisfies(query).Should().BeTrue();
+        }
+
+        [Fact]
+        public void TrueCorrectlyReturnsFalse()
+        {
+            var query = Query<bool>.Is.True();
+
+            false.Satisfies(query).Should().BeFalse();
+        }
+
+        [Fact]
+        public void FalseCorrectlyReturnsTrue()
+        {
+            var query = Query<bool>.Is.False();
+
+            false.Satisfies(query).Should().BeTrue();
+        }
+
+        [Fact]
+        public void FalseCorrectlyReturnsFalse()
+        {
+            var query = Query<bool>.Is.False();
+
+            true.Satisfies(query).Should().BeFalse();
+        }
+    }
+}
