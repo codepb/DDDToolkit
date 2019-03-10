e475e22e2ba27d52d6f758e9e8af27c38b286f9b
codepb <paul.j.burgess@outlook.com>
2019-03-10 18:04:27 +0000

    Adding xml documentation comments to the core project.

--- src/DDDToolkit.Core/Interfaces/IValueObject.cs
+++ src/DDDToolkit.Core/Interfaces/IValueObject.cs
@@ -2,6 +2,23 @@
 
 namespace DDDToolkit.Core.Interfaces
 {
+    /// <summary>
+    /// <para>
+    /// An object that has no conceptual identity. It is
+    /// defined by its properties
+    /// </para>
+    /// <para>
+    /// Two value objects are identical if they are the same type
+    /// and all of their properties are equal.
+    /// </para>
+    /// <para>
+    /// It is recommended that you make value objects immutable.
+    /// As a value object is defined by its properties, you should
+    /// not change the values on the value object, but instead
+    /// create a new value object with the desired properties
+    /// a new object.
+    /// </para>
+    /// </summary>
     public interface IValueObject : IEquatable<IValueObject>
     {
     }
